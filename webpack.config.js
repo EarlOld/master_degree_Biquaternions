6938143e2d9cdbb23384ea1b202f71d1cbeb263d
earlold <dimonsuper010@gmail.com>
2021-12-08 01:26:01 +0200

    Created production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,51 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devMode = process.env.NODE_ENV !== "production";
+
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
+  mode: "production",
   devtool: "inline-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimize: true,
+    runtimeChunk: "single",
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 20000,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `npm.${packageName.replace("@", "")}`;
+          },
+        },
+      },
+    },
+  },
+  output: {
+    filename: "[contenthash].js",
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
@@ -49,13 +88,28 @@ module.exports = {
       },
     ],
   },
-  plugins: [].concat(
-    devMode
-      ? []
-      : [
-          new MiniCssExtractPlugin({
-            filename: "main.css",
-          }),
-        ]
-  ),
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "index.ejs",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: false,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+      inject: true,
+      // Note that you can add custom options here if you need to handle other custom logic in index.html
+      // To track JavaScript errors via TrackJS, sign up for a free trial at TrackJS.com and enter your token below.
+      trackJSToken: "",
+    }),
+  ]
 };
